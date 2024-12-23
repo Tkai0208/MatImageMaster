c471b7c645bbcc2995546c1d112e61fa9c192bbe
Tkai0208 <2190564353@qq.com>
2024-12-23 16:52:43 +0800

    Add files via upload

--- MatlmageMaster_exported.m
+++ MatlmageMaster_exported.m
@@ -2,21 +2,21 @@ classdef MatlmageMaster_exported < matlab.apps.AppBase
 
     % Properties that correspond to app components
     properties (Access = public)
-        UIFigure                       matlab.ui.Figure
-        extractHOGFeatures_CallbackButton  matlab.ui.control.Button
-        extractLBPFeatures_CallbackButton  matlab.ui.control.Button
-        extractTarget_CallbackButton   matlab.ui.control.Button
-        edgeDetection_CallbackButton   matlab.ui.control.Button
-        noiseParamEditField            matlab.ui.control.EditField
-        noiseParamEditFieldLabel       matlab.ui.control.Label
-        noiseAndFilter_CallbackButton  matlab.ui.control.Button
-        noiseTypeDropDown              matlab.ui.control.DropDown
-        noiseTypeDropDownLabel         matlab.ui.control.Label
-        transformImage_CallbackButton  matlab.ui.control.Button
-        contrastEnhancement_CallbackButton  matlab.ui.control.Button
-        histogramOperations_CallbackButton  matlab.ui.control.Button
-        open_pictureButton             matlab.ui.control.StateButton
-        imageAxes                      matlab.ui.control.UIAxes
+        UIFigure        matlab.ui.Figure
+        HOGButton       matlab.ui.control.Button
+        LBPButton       matlab.ui.control.Button
+        Button_7        matlab.ui.control.Button
+        Button_6        matlab.ui.control.Button
+        EditField       matlab.ui.control.EditField
+        EditFieldLabel  matlab.ui.control.Label
+        Button_5        matlab.ui.control.Button
+        DropDown        matlab.ui.control.DropDown
+        DropDownLabel   matlab.ui.control.Label
+        Button_2        matlab.ui.control.Button
+        Button_4        matlab.ui.control.Button
+        Button_3        matlab.ui.control.Button
+        Button          matlab.ui.control.StateButton
+        imageAxes       matlab.ui.control.UIAxes
     end
 
 
@@ -24,8 +24,8 @@ classdef MatlmageMaster_exported < matlab.apps.AppBase
     % Callbacks that handle component events
     methods (Access = private)
 
-        % Value changed function: open_pictureButton
-        function open_pictureButtonValueChanged(app, event)
+        % Value changed function: Button
+        function ButtonValueChanged(app, event)
             [file, path] = uigetfile({'*.png;*.jpg;*.jpeg', 'Images'});
             if isequal(file, 0)
                 return;
@@ -40,8 +40,8 @@ classdef MatlmageMaster_exported < matlab.apps.AppBase
             setappdata(app.UIFigure, 'ImagePath', imagePath);
         end
 
-        % Button pushed function: histogramOperations_CallbackButton
-        function histogramOperations_CallbackButtonPushed(app, event)
+        % Button pushed function: Button_3
+        function Button_3Pushed(app, event)
             % 获取存储的图像路径
             imagePath = getappdata(app.UIFigure, 'ImagePath');
             if isempty(imagePath)
@@ -59,8 +59,8 @@ classdef MatlmageMaster_exported < matlab.apps.AppBase
             end
         end
 
-        % Button pushed function: contrastEnhancement_CallbackButton
-        function contrastEnhancement_CallbackButtonPushed(app, event)
+        % Button pushed function: Button_4
+        function Button_4Pushed(app, event)
             % 获取存储的图像路径
             imagePath = getappdata(app.UIFigure, 'ImagePath');
             if isempty(imagePath)
@@ -78,8 +78,8 @@ classdef MatlmageMaster_exported < matlab.apps.AppBase
             end
         end
 
-        % Button pushed function: transformImage_CallbackButton
-        function transformImage_CallbackButtonPushed(app, event)
+        % Button pushed function: Button_2
+        function Button_2Pushed(app, event)
             % 获取存储的图像路径
             imagePath = getappdata(app.UIFigure, 'ImagePath');
             if isempty(imagePath)
@@ -97,8 +97,8 @@ classdef MatlmageMaster_exported < matlab.apps.AppBase
             end
         end
 
-        % Value changed function: noiseTypeDropDown
-        function noiseTypeDropDownValueChanged(app, event)
+        % Value changed function: DropDown
+        function DropDownValueChanged(app, event)
             % 获取存储的图像路径
             imagePath = getappdata(app.UIFigure, 'ImagePath');
             if isempty(imagePath)
@@ -107,10 +107,10 @@ classdef MatlmageMaster_exported < matlab.apps.AppBase
             end
 
             % 获取用户选择的噪声类型
-            noiseType = app.noiseTypeDropDown.Value;
+            noiseType = app.DropDown.Value;
 
             % 获取用户输入的噪声参数
-            noiseParam = str2double(app.noiseParamEditField.Value);
+            noiseParam = str2double(app.EditField.Value);
             if isnan(noiseParam)
                 warndlg('请输入有效的噪声参数', '无效参数');
                 return;
@@ -125,8 +125,8 @@ classdef MatlmageMaster_exported < matlab.apps.AppBase
             end
         end
 
-        % Button pushed function: noiseAndFilter_CallbackButton
-        function noiseAndFilter_CallbackButtonPushed(app, event)
+        % Button pushed function: Button_5
+        function Button_5Pushed(app, event)
             % 获取存储的图像路径
             imagePath = getappdata(app.UIFigure, 'ImagePath');
             if isempty(imagePath)
@@ -135,10 +135,10 @@ classdef MatlmageMaster_exported < matlab.apps.AppBase
             end
 
             % 获取用户选择的噪声类型
-            noiseType = app.noiseTypeDropDown.Value;
+            noiseType = app.DropDown.Value;
 
             % 获取用户输入的噪声参数
-            noiseParam = str2double(app.noiseParamEditField.Value);
+            noiseParam = str2double(app.EditField.Value);
             if isnan(noiseParam)
                 warndlg('请输入有效的噪声参数', '无效参数');
                 return;
@@ -153,8 +153,8 @@ classdef MatlmageMaster_exported < matlab.apps.AppBase
             end
         end
 
-        % Button pushed function: edgeDetection_CallbackButton
-        function edgeDetection_CallbackButtonPushed(app, event)
+        % Button pushed function: Button_6
+        function Button_6Pushed(app, event)
             % 获取存储的图像路径
             imagePath = getappdata(app.UIFigure, 'ImagePath');
             if isempty(imagePath)
@@ -172,8 +172,8 @@ classdef MatlmageMaster_exported < matlab.apps.AppBase
             end
         end
 
-        % Button pushed function: extractTarget_CallbackButton
-        function extractTarget_CallbackButtonPushed(app, event)
+        % Button pushed function: Button_7
+        function Button_7Pushed(app, event)
             % 获取存储的图像路径
             imagePath = getappdata(app.UIFigure, 'ImagePath');
             if isempty(imagePath)
@@ -191,8 +191,8 @@ classdef MatlmageMaster_exported < matlab.apps.AppBase
             end
         end
 
-        % Button pushed function: extractLBPFeatures_CallbackButton
-        function extractLBPFeatures_CallbackButtonPushed(app, event)
+        % Button pushed function: LBPButton
+        function LBPButtonPushed(app, event)
             % 获取存储的图像路径和提取的目标图像路径
             originalImagePath = getappdata(app.UIFigure, 'ImagePath');
             targetImagePath = fullfile(fileparts(originalImagePath), 'extracted_target.png');
@@ -212,8 +212,8 @@ classdef MatlmageMaster_exported < matlab.apps.AppBase
             end
         end
 
-        % Button pushed function: extractHOGFeatures_CallbackButton
-        function extractHOGFeatures_CallbackButtonPushed(app, event)
+        % Button pushed function: HOGButton
+        function HOGButtonPushed(app, event)
             % 获取存储的图像路径和提取的目标图像路径
             originalImagePath = getappdata(app.UIFigure, 'ImagePath');
             targetImagePath = fullfile(fileparts(originalImagePath), 'extracted_target.png');
@@ -242,90 +242,123 @@ classdef MatlmageMaster_exported < matlab.apps.AppBase
 
             % Create UIFigure and hide until all components are created
             app.UIFigure = uifigure('Visible', 'off');
-            app.UIFigure.Position = [100 100 752 506];
+            app.UIFigure.Position = [100 100 804 610];
             app.UIFigure.Name = 'MATLAB App';
 
             % Create imageAxes
             app.imageAxes = uiaxes(app.UIFigure);
             title(app.imageAxes, '图片显示')
-            app.imageAxes.Position = [11 1 382 493];
+            app.imageAxes.Position = [1 43 485 562];
 
-            % Create open_pictureButton
-            app.open_pictureButton = uibutton(app.UIFigure, 'state');
-            app.open_pictureButton.ValueChangedFcn = createCallbackFcn(app, @open_pictureButtonValueChanged, true);
-            app.open_pictureButton.Text = 'open_picture';
-            app.open_pictureButton.Position = [414 428 100 23];
+            % Create Button
+            app.Button = uibutton(app.UIFigure, 'state');
+            app.Button.ValueChangedFcn = createCallbackFcn(app, @ButtonValueChanged, true);
+            app.Button.Text = '打开图像';
+            app.Button.FontSize = 18;
+            app.Button.FontWeight = 'bold';
+            app.Button.Position = [516 549 100 30];
 
-            % Create histogramOperations_CallbackButton
-            app.histogramOperations_CallbackButton = uibutton(app.UIFigure, 'push');
-            app.histogramOperations_CallbackButton.ButtonPushedFcn = createCallbackFcn(app, @histogramOperations_CallbackButtonPushed, true);
-            app.histogramOperations_CallbackButton.Position = [414 391 179 23];
-            app.histogramOperations_CallbackButton.Text = 'histogramOperations_Callback';
+            % Create Button_3
+            app.Button_3 = uibutton(app.UIFigure, 'push');
+            app.Button_3.ButtonPushedFcn = createCallbackFcn(app, @Button_3Pushed, true);
+            app.Button_3.FontSize = 18;
+            app.Button_3.FontWeight = 'bold';
+            app.Button_3.Position = [516 507 105 30];
+            app.Button_3.Text = '直方图操作';
 
-            % Create contrastEnhancement_CallbackButton
-            app.contrastEnhancement_CallbackButton = uibutton(app.UIFigure, 'push');
-            app.contrastEnhancement_CallbackButton.ButtonPushedFcn = createCallbackFcn(app, @contrastEnhancement_CallbackButtonPushed, true);
-            app.contrastEnhancement_CallbackButton.Position = [414 357 184 23];
-            app.contrastEnhancement_CallbackButton.Text = 'contrastEnhancement_Callback';
+            % Create Button_4
+            app.Button_4 = uibutton(app.UIFigure, 'push');
+            app.Button_4.ButtonPushedFcn = createCallbackFcn(app, @Button_4Pushed, true);
+            app.Button_4.FontSize = 18;
+            app.Button_4.FontWeight = 'bold';
+            app.Button_4.Position = [516 465 105 30];
+            app.Button_4.Text = '对比度增强';
 
-            % Create transformImage_CallbackButton
-            app.transformImage_CallbackButton = uibutton(app.UIFigure, 'push');
-            app.transformImage_CallbackButton.ButtonPushedFcn = createCallbackFcn(app, @transformImage_CallbackButtonPushed, true);
-            app.transformImage_CallbackButton.Position = [414 320 152 23];
-            app.transformImage_CallbackButton.Text = 'transformImage_Callback';
+            % Create Button_2
+            app.Button_2 = uibutton(app.UIFigure, 'push');
+            app.Button_2.ButtonPushedFcn = createCallbackFcn(app, @Button_2Pushed, true);
+            app.Button_2.FontSize = 18;
+            app.Button_2.FontWeight = 'bold';
+            app.Button_2.Position = [516 424 207 30];
+            app.Button_2.Text = '变换图像(旋转，缩小等)';
 
-            % Create noiseTypeDropDownLabel
-            app.noiseTypeDropDownLabel = uilabel(app.UIFigure);
-            app.noiseTypeDropDownLabel.HorizontalAlignment = 'right';
-            app.noiseTypeDropDownLabel.Position = [414 273 116 22];
-            app.noiseTypeDropDownLabel.Text = 'noiseTypeDropDown';
+            % Create DropDownLabel
+            app.DropDownLabel = uilabel(app.UIFigure);
+            app.DropDownLabel.HorizontalAlignment = 'right';
+            app.DropDownLabel.FontSize = 18;
+            app.DropDownLabel.FontWeight = 'bold';
+            app.DropDownLabel.FontColor = [0 0.4471 0.7412];
+            app.DropDownLabel.Position = [516 360 77 23];
+            app.DropDownLabel.Text = '噪声类型';
 
-            % Create noiseTypeDropDown
-            app.noiseTypeDropDown = uidropdown(app.UIFigure);
-            app.noiseTypeDropDown.Items = {'gaussian', 'salt & pepper', 'speckle'};
-            app.noiseTypeDropDown.ValueChangedFcn = createCallbackFcn(app, @noiseTypeDropDownValueChanged, true);
-            app.noiseTypeDropDown.Position = [545 273 100 22];
-            app.noiseTypeDropDown.Value = 'gaussian';
+            % Create DropDown
+            app.DropDown = uidropdown(app.UIFigure);
+            app.DropDown.Items = {'gaussian', 'salt & pepper', 'speckle'};
+            app.DropDown.ValueChangedFcn = createCallbackFcn(app, @DropDownValueChanged, true);
+            app.DropDown.FontSize = 18;
+            app.DropDown.FontWeight = 'bold';
+            app.DropDown.FontColor = [0 0.4471 0.7412];
+            app.DropDown.Position = [608 359 100 24];
+            app.DropDown.Value = 'gaussian';
 
-            % Create noiseAndFilter_CallbackButton
-            app.noiseAndFilter_CallbackButton = uibutton(app.UIFigure, 'push');
-            app.noiseAndFilter_CallbackButton.ButtonPushedFcn = createCallbackFcn(app, @noiseAndFilter_CallbackButtonPushed, true);
-            app.noiseAndFilter_CallbackButton.Position = [414 210 144 23];
-            app.noiseAndFilter_CallbackButton.Text = 'noiseAndFilter_Callback';
+            % Create Button_5
+            app.Button_5 = uibutton(app.UIFigure, 'push');
+            app.Button_5.ButtonPushedFcn = createCallbackFcn(app, @Button_5Pushed, true);
+            app.Button_5.FontSize = 18;
+            app.Button_5.FontWeight = 'bold';
+            app.Button_5.FontColor = [0 0.4471 0.7412];
+            app.Button_5.Position = [516 291 141 30];
+            app.Button_5.Text = '添加噪声和过滤';
 
-            % Create noiseParamEditFieldLabel
-            app.noiseParamEditFieldLabel = uilabel(app.UIFigure);
-            app.noiseParamEditFieldLabel.HorizontalAlignment = 'right';
-            app.noiseParamEditFieldLabel.Position = [414 243 116 22];
-            app.noiseParamEditFieldLabel.Text = 'noiseParamEditField';
+            % Create EditFieldLabel
+            app.EditFieldLabel = uilabel(app.UIFigure);
+            app.EditFieldLabel.HorizontalAlignment = 'right';
+            app.EditFieldLabel.FontSize = 18;
+            app.EditFieldLabel.FontWeight = 'bold';
+            app.EditFieldLabel.FontColor = [0 0.4471 0.7412];
+            app.EditFieldLabel.Position = [516 330 77 23];
+            app.EditFieldLabel.Text = '噪声参数';
 
-            % Create noiseParamEditField
-            app.noiseParamEditField = uieditfield(app.UIFigure, 'text');
-            app.noiseParamEditField.Position = [545 243 100 22];
+            % Create EditField
+            app.EditField = uieditfield(app.UIFigure, 'text');
+            app.EditField.FontSize = 18;
+            app.EditField.FontWeight = 'bold';
+            app.EditField.FontColor = [0 0.4471 0.7412];
+            app.EditField.Position = [608 326 100 27];
 
-            % Create edgeDetection_CallbackButton
-            app.edgeDetection_CallbackButton = uibutton(app.UIFigure, 'push');
-            app.edgeDetection_CallbackButton.ButtonPushedFcn = createCallbackFcn(app, @edgeDetection_CallbackButtonPushed, true);
-            app.edgeDetection_CallbackButton.Position = [414 159 145 23];
-            app.edgeDetection_CallbackButton.Text = 'edgeDetection_Callback';
+            % Create Button_6
+            app.Button_6 = uibutton(app.UIFigure, 'push');
+            app.Button_6.ButtonPushedFcn = createCallbackFcn(app, @Button_6Pushed, true);
+            app.Button_6.FontSize = 18;
+            app.Button_6.FontWeight = 'bold';
+            app.Button_6.Position = [516 232 100 30];
+            app.Button_6.Text = '边缘检测';
 
-            % Create extractTarget_CallbackButton
-            app.extractTarget_CallbackButton = uibutton(app.UIFigure, 'push');
-            app.extractTarget_CallbackButton.ButtonPushedFcn = createCallbackFcn(app, @extractTarget_CallbackButtonPushed, true);
-            app.extractTarget_CallbackButton.Position = [414 121 140 23];
-            app.extractTarget_CallbackButton.Text = ' extractTarget_Callback';
+            % Create Button_7
+            app.Button_7 = uibutton(app.UIFigure, 'push');
+            app.Button_7.ButtonPushedFcn = createCallbackFcn(app, @Button_7Pushed, true);
+            app.Button_7.FontSize = 18;
+            app.Button_7.FontWeight = 'bold';
+            app.Button_7.Position = [516 188 100 30];
+            app.Button_7.Text = '提取目标';
 
-            % Create extractLBPFeatures_CallbackButton
-            app.extractLBPFeatures_CallbackButton = uibutton(app.UIFigure, 'push');
-            app.extractLBPFeatures_CallbackButton.ButtonPushedFcn = createCallbackFcn(app, @extractLBPFeatures_CallbackButtonPushed, true);
-            app.extractLBPFeatures_CallbackButton.Position = [414 85 174 23];
-            app.extractLBPFeatures_CallbackButton.Text = 'extractLBPFeatures_Callback';
+            % Create LBPButton
+            app.LBPButton = uibutton(app.UIFigure, 'push');
+            app.LBPButton.ButtonPushedFcn = createCallbackFcn(app, @LBPButtonPushed, true);
+            app.LBPButton.FontSize = 18;
+            app.LBPButton.FontWeight = 'bold';
+            app.LBPButton.FontColor = [0.4941 0.1843 0.5569];
+            app.LBPButton.Position = [516 144 123 30];
+            app.LBPButton.Text = '提取LBP特征';
 
-            % Create extractHOGFeatures_CallbackButton
-            app.extractHOGFeatures_CallbackButton = uibutton(app.UIFigure, 'push');
-            app.extractHOGFeatures_CallbackButton.ButtonPushedFcn = createCallbackFcn(app, @extractHOGFeatures_CallbackButtonPushed, true);
-            app.extractHOGFeatures_CallbackButton.Position = [414 46 178 23];
-            app.extractHOGFeatures_CallbackButton.Text = 'extractHOGFeatures_Callback';
+            % Create HOGButton
+            app.HOGButton = uibutton(app.UIFigure, 'push');
+            app.HOGButton.ButtonPushedFcn = createCallbackFcn(app, @HOGButtonPushed, true);
+            app.HOGButton.FontSize = 18;
+            app.HOGButton.FontWeight = 'bold';
+            app.HOGButton.FontColor = [0.4941 0.1843 0.5569];
+            app.HOGButton.Position = [516 101 128 30];
+            app.HOGButton.Text = '提取HOG特征';
 
             % Show the figure after all components are created
             app.UIFigure.Visible = 'on';
